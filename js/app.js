b2e611b6cb12d2ac4a0d1616f10be504f9906c5a
mghazal1024 <mghazal1024@gmail.com>
2021-08-30 09:20:45 +0200

    initial commit - basic html and css done

---
+++ js/app.js
@@ -0,0 +1,38 @@
+
+// const ingredientsDropdown = document.querySelector('.tags-dropdown--ingredients');
+
+// ingredientsDropdown.addEventListener('click', () => {
+//     ingredientsDropdown.classList.add('opened')
+// })
+
+document.addEventListener('click', (e) => {
+    const ingredientsDropdown = document.querySelector('.tags-dropdown--ingredients');
+    const ingredientsInput = document.querySelector('.tags-dropdown--ingredients input');
+    
+    const appareilDropdown = document.querySelector('.tags-dropdown--appareil');
+    const appareilInput = document.querySelector('.tags-dropdown--appareil input');
+
+    const ustensilesDropdown = document.querySelector('.tags-dropdown--ustensiles');
+    const ustensilesInput = document.querySelector('.tags-dropdown--ustensiles input')
+    let targetElement = e.target;
+    
+    if(targetElement == ingredientsInput) {
+        ingredientsDropdown.classList.add('opened');
+    } else {
+        ingredientsDropdown.classList.remove('opened');
+    }
+
+    if(targetElement == appareilInput) {
+        appareilDropdown.classList.add('opened');
+    } else {
+        appareilDropdown.classList.remove('opened');
+    }
+
+    if(targetElement == ustensilesInput) {
+        ustensilesDropdown.classList.add('opened');
+    } else {
+        ustensilesDropdown.classList.remove('opened');
+    }
+
+
+})
